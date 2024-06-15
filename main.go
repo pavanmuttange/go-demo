4467cdba142f78d542c7ea80414da2ddf9f59238
pavanmuttange <88590475+pavanmuttange@users.noreply.github.com>
2024-06-15 16:48:40 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-  "fmt"
+	"demo/controller"
+	"demo/pkg/config"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main(){
-fmt.Println("hello")
+func main() {
+
+	config.Init()
+	router := gin.Default()
+	router.GET("/health", controller.Ping)
+	router.GET("/create", controller.CreateTable)
+	router.POST("/students", controller.CreateStudent)
+	router.PUT("/students/:id", controller.UpdateStudent)
+	router.DELETE("/students/:id", controller.DeleteStudent)
+	router.GET("/students/:id", controller.GetStudentByID)
+	router.GET("/students", controller.GetAllStudents)
+	router.Run(":8001")
 }
